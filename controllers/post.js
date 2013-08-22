23dfc3cd75bae8968145269e0f7889cd4a23e2c8
Rafael Pinheiro <moriarty.pinheiro@gmail.com>
2013-08-22 10:49:10 -0300

    fixed views and created the talk view

--- controllers/post.js
+++ controllers/post.js
@@ -14,9 +14,31 @@ exports.index = function(req, res){
 		if (err) {
 			res.send('Database error', 500);
 		} else {
-			console.log(posts);
-			res.render('index', { posts: posts });
+			res.send(posts);
 		}
 	});
-  
+};
+
+
+/*
+ * POST post
+ */
+exports.create = function(req, res){
+	var Talk = req.model('talk')
+	  ,	talkId = req.param('talk');
+	
+	var post = {
+		author: req.user[0]._id,
+		body: req.param('body'),
+		date: new Date()
+	};
+
+
+	Talk.update({_id: new ObjectID(talkId)}, {'$push': {posts: post}}, function (err, talk) {
+		if (err) {
+			res.send('Database error', 500);
+		} else {
+			res.send(post);
+		}
+	});
 };
