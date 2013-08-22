2de3eabf7808d697a51a962e55584a9b04973551
Rafael Pinheiro <moriarty.pinheiro@gmail.com>
2013-08-22 12:04:45 -0300

    single talk

--- models/talk.js
+++ models/talk.js
@@ -1,6 +1,7 @@
-var baseModel = require('./baseModel');
-
-var model = baseModel('talk');
+var baseModel = require('./baseModel')
+  ,	User = require('./user')
+  , ObjectID = require('mongodb').ObjectID
+  ,	model = baseModel('talk');
 
 model.getPosts = function (filter, cb) {
 	var since = filter.since;
@@ -13,5 +14,42 @@ model.getPosts = function (filter, cb) {
 		cb(err, posts);
 	});
 }
+
+model._findOne = model.findOne;
+
+model.findOne = function (filter, cb) {
+	this._findOne(filter, function (err, item) {
+		if (err) {
+			cb(err, item);
+		} else {
+
+			var authors = [];
+			for (var i = 0; i < item.posts.length; i++) {
+				authors.push(new ObjectID(item.posts[i].author + ''));
+			}
+			
+			User.find({_id: {'$in': authors}}, function (err, items) {
+				item.posts.map(function (single) {
+					var author = single.author;
+
+
+					for (var i = 0; i < items.length; i++) {
+						if (items[i]._id.toString() === author.toString()) {
+							single.author = items[i];
+							break;
+						}
+					}
+					return single;
+					
+				});
+				
+				cb(err, item);
+				
+			});
+
+		}
+
+	});
+};
 
 module.exports = model;
