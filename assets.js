dcc28836ccfadfcb66c1942952e8a338b87691e9
Rafael Pinheiro <moriarty.pinheiro@gmail.com>
2013-08-25 18:10:43 -0300

    Making some changes to be more mobile friendly

--- assets.js
+++ assets.js
@@ -1,9 +1,10 @@
 module.exports = function(assets) {
     assets.root = __dirname + '/public';
 
-    assets.addCss('css/style.styl');
+    
     assets.addCss('bootstrap/css/bootstrap.min.css');
     assets.addCss('bootstrap/css/bootstrap-responsive.min.css');
+    assets.addCss('css/style.styl');
 
     assets.addJs('js/jquery.js');
 }
